b82555f86832d2fcb3a98fb197b83ad56d14f777
jain-anshu <anshuj@gmail.com>
2023-04-03 10:28:40 -0700

    Adds gitignore; Builds React components; Installs react, reactDOM

---
+++ apr/feShare/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("root"))
+   const h1 = React.createElement("h1", {}, "Hello React")
+   root.render(h1)
