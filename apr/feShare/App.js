f13a8aa608c76212e41b4170c8e353d5227c9633
jain-anshu <anshuj@gmail.com>
2023-04-06 20:58:46 -0700

    Adds stylesheet

--- apr/feShare/App.js
+++ apr/feShare/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"))
-   const h1 = React.createElement("h1", {}, "Hello React")
-   root.render(h1)
+const h2 = React.createElement("h2", {}, "Learn React fast!")
+const h1 = React.createElement("h1", {}, "Hello React")
+const div = React.createElement("div", {}, [h1, h2])
+root.render(
+   div
+);
