350b0152bbc43c0d4730e65f749a2da4e0f2b980
jain-anshu <anshuj@gmail.com>
2023-08-11 13:28:13 -0700

    Builds header page; adds stylesheets

--- Documents/july/pantry_stocker_fe/src/Header.js
+++ Documents/july/pantry_stocker_fe/src/Header.js
@@ -1,4 +1,14 @@
 
 export const Header = function () {
-    return <h1 className="Header"> Pantry Stocker </h1>
+    return (
+        <div className="Header">
+            < div className="Title"> <h1>Pantry Stocker </h1></div >
+            <ul className="Menu">
+                <li className="MenuItem">Shopping list</li>
+                <li className="MenuItem">Ingredients</li>
+                <li className="MenuItem">Stores</li>
+                <li className="MenuItem">Categories</li>
+            </ul>
+        </div>
+    );
 }
