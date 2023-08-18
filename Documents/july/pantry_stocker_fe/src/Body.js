2b83cfd991208d709395f5ff496fff941bb69395
jain-anshu <anshuj@gmail.com>
2023-08-17 18:47:57 -0700

    Updated fields on Store form

--- Documents/july/pantry_stocker_fe/src/Body.js
+++ Documents/july/pantry_stocker_fe/src/Body.js
@@ -12,6 +12,13 @@ export const Body = function () {
         quantity: 0,
         alsoKnownAs: ""
     });
+
+    const [store, setStore] = useState({
+        name: "",
+        has_website: false,
+        has_delivery: false,
+        website_url: ""
+    });
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -71,25 +78,21 @@ export const Body = function () {
                 <form className="Form">
 
                     <label className="FormField">Store Name
-                        <input placeholder="Enter Ingredient name" value={ingredient.name} onChange={(e) => setIngredient({ ...ingredient, name: e.target.value })} className="FormField">
+                        <input placeholder="Enter Store name" value={store.name} onChange={(e) => setStore({ ...ingredient, name: e.target.value })} className="FormField">
                         </input>
                     </label>
-                    <label className="FormField">Is this a basic ingredient?
-                        <input type="checkbox" checked={ingredient.is_basic} onChange={(e) => setIngredient({ ...ingredient, is_basic: e.target.checked })} className="FormField">
+                    <label className="FormField">Does it have a website?
+                        <input type="checkbox" checked={store.has_website} onChange={(e) => setStore({ ...store, has_website: e.target.checked })} className="FormField">
                         </input>
                     </label>
-                    <label className="FormField">Is this a family favorite?
-                        <input type="checkbox" checked={ingredient.is_family_favorite} onChange={(e) => setIngredient({ ...ingredient, is_family_favorite: e.target.checked })} className="FormField">
+                    <label className="FormField">Does it deliver via instacart?
+                        <input type="checkbox" checked={store.has_delivery} onChange={(e) => setStore({ ...store, has_delivery: e.target.checked })} className="FormField">
                         </input>
                     </label>
-                    <label className="FormField">How much of this ingredient is left?
-                        <select value={ingredient.status} onChange={(e) => setIngredient({ ...ingredient, status: e.target.value })}>
-                            <option value={1}> Enough</option>
-                            <option value={2}> Some</option>
-                            <option value={3}> None</option>
-                        </select>
-
-                    </label>
+                    {store.has_website && <label className="FormField">Website URL
+                        <input placeholder="Enter URL" value={store.name} onChange={(e) => setStore({ ...store, name: e.target.value })} className="FormField">
+                        </input>
+                    </label>}
                     <button onClick={handleSubmit} className="Button">Add</button>
                 </form >
             </div>
